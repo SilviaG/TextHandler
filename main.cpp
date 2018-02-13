21e68862f873061d7803c34baf8d8b2eed7287ed
Silvia <slv.giannini@gmail.com>
2018-02-13 20:26:56 +0100

    Update main.cpp
    Add tolower conversion for not case-sensitive token search.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 #include <string.h>
+#include <algorithm>
 
 using namespace std;
 
-static unsigned int TokenCount (std::string const &text, std::string const &token)
+static unsigned int TokenCount (string const &input_text, string const &input_token)
 {
   unsigned int count = 0;
+
+  string text = input_text;
+  string token = input_token;
+    
+  transform(input_text.begin(), input_text.end(), text.begin(), ::tolower);
+  transform(input_token.begin(), input_token.end(), token.begin(), ::tolower);
 
   for (int i = 0; i <= (text.length () - token.length ()); i++)
   {
@@ -19,9 +26,9 @@ int main ()
 {
   cout << "Find the number of occurrences of a lexeme in a text." << endl;
 
-  std::string text =
+  string text =
     "Enter here the text that has to be analysed for searching the characters string.";
-  std::string token = "the";
+  string token = "THE";
 
   cout << "Text: " << text << endl;
   cout << "Token: " << token << endl;
